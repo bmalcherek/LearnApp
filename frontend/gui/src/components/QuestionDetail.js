e3ae73b975aca018f6d32b5292740f4556d72d2a
lordmalcher <bartoszmalcherek@outlook.com>
2019-07-10 17:15:10 +0200

    questionDetail detect if it's an image, but displaying an image not yet working

--- frontend/gui/src/components/QuestionDetail.js
+++ frontend/gui/src/components/QuestionDetail.js
@@ -22,11 +22,26 @@ export class QuestionDetail extends Component {
     }
 
     render() {
+        const { Meta } = Card;
+        let card;
+        if (this.state.question.is_image) {
+            card = (<Card
+                style={{ width: 300 }}
+                cover={
+                    <img
+                        alt="example"
+                        src={this.state.question.image_src} />}>
+                <Meta title={this.state.question.question} description={this.state.question.answer} />
+            </Card>);
+        } else {
+            card = (<Card title={this.state.question.question}>
+                <p>{this.state.question.answer}</p>
+            </Card>);
+        }
+
         return (
             <div>
-                <Card title={this.state.question.question}>
-                    <p>{this.state.question.answer}</p>
-                </Card>
+                {card}
             </div>
         );
     }
