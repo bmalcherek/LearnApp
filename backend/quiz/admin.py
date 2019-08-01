6117f616277e5e32054d4027298bbded604dcd39
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2019-08-01 13:15:44 +0200

    Switch to backend submodule

--- backend/quiz/admin.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from .models import Question, Collection, MyCollections, MyQuestions
-
-admin.site.register(Question)
-admin.site.register(Collection)
-admin.site.register(MyCollections)
-admin.site.register(MyQuestions)
